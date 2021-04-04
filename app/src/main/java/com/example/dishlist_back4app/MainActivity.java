5e6a9ea0e350f3541c79b2c9d59a6fd1e39ae35b
bincem98 <vmadamba29@gmail.com>
2021-04-04 10:23:07 -0700

    details,settings,nav bar

--- app/src/main/java/com/example/dishlist_back4app/MainActivity.java
+++ app/src/main/java/com/example/dishlist_back4app/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.dishlist_back4app;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Button btnLogOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        btnLogOut = findViewById(R.id.btnLogOut);
+
+        // Action Bar Color Change
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#ffcc4c"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+        /* code for Bottom Navigation Bar *NOT WORKING YET* */
+        /*
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_add:
+                        Toast.makeText(MainActivity.this, "Add", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });*/
     }
 }
