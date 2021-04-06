b15ab38299fae08b01669f472bd1a8adb05d7477
Zachary Himed <zackhimed7@gmail.com>
2021-04-05 19:44:53 -0700

    Create Recipe/Post Model

---
+++ app/src/main/java/Model/Post.java
@@ -0,0 +1,54 @@
+package Model;
+
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("TodoItem")
+
+public class Post extends ParseObject {
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "user";
+    public static final String KEY_INGREDIENTS = "ingredients";
+    public static final String KEY_RECIPENAME="recipeName";
+
+    public String getIngredients(){
+        return getString(KEY_INGREDIENTS);
+    }
+
+    public void setIngredients(String ingredients){
+        put(KEY_INGREDIENTS,ingredients);
+    }
+
+    public String getRecipeName() {
+        return getString(KEY_RECIPENAME);
+    }
+
+    public void setRecipeName(String name){
+        put(KEY_RECIPENAME,name);
+    }
+
+    public String getDescription(){
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description){
+        put(KEY_DESCRIPTION,description);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_IMAGE);
+    }
+    public void setImage(ParseFile parseFile){
+        put(KEY_IMAGE,parseFile);
+    }
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+    public void setUser(ParseUser user){
+        put(KEY_USER,user);
+    }
+}
---
+++ app/src/main/java/com/example/dishlist_back4app/CreateRecipeActivity.java
@@ -0,0 +1,23 @@
+package com.example.dishlist_back4app;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateRecipeActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate( Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_addrecipe);
+
+
+
+
+    }
+}
