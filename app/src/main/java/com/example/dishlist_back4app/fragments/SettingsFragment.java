6c8cdb2c641a6fd3bfbdbbd40d762bbc02ba153c
bincem98 <vmadamba29@gmail.com>
2021-04-10 14:57:15 -0700

    logout

--- app/src/main/java/com/example/dishlist_back4app/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/dishlist_back4app/fragments/SettingsFragment.java
@@ -1,6 +1,49 @@
 package com.example.dishlist_back4app.fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.dishlist_back4app.LoginActivity;
+import com.example.dishlist_back4app.MainActivity;
+import com.example.dishlist_back4app.R;
+import com.example.dishlist_back4app.SignUpActivity;
+import com.parse.Parse;
+import com.parse.ParseUser;
+
 public class SettingsFragment extends Fragment {
+    public static final String TAG = "SettingsFragment";
+    private Button btnLogOut;
+
+    public SettingsFragment() {
+        //empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        btnLogOut = view.findViewById(R.id.btnLogOut);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Toast.makeText(getActivity(), "Successfully Logged Out", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getActivity(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
+    }
 }
